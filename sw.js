aee856cfa95d2e0fb8084714231b6330b6f43a0d
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 18:04:36 +0000

    v14: عدد فارسی + مخفی شدن صفحه صفر

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v13';
+const CACHE = 'karkard-v14';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
