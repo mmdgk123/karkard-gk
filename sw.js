5ea6fc0fa72b1216b998453453d636309ce20937
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 17:15:25 +0000

    دارک مود + ورژن v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v1';
+const CACHE = 'karkard-v2';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
