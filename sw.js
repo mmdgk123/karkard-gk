bfaa3a0c0ac956f2ec37ec258cf065d4b1a7ead5
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 18:49:23 +0000

    v22: فیکس پرش تب‌ها هنگام اسکرول/کیبورد

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v21';
+const CACHE = 'karkard-v22';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
