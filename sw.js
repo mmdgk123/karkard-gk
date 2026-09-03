fa5f80fb26b3d19bfe890b8c753d8db6f79a8741
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 18:59:40 +0000

    v24: دکمه امروز + فرمت خودکار تاریخ و جداکننده هزارگان مبلغ

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v23';
+const CACHE = 'karkard-v24';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
