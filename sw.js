93ecec46a79da1d28ef5f0b4b193a61e767ebc4c
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 17:44:29 +0000

    v8: مبلغ جداگانه هر آیتم موقع ثبت

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v7';
+const CACHE = 'karkard-v8';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
