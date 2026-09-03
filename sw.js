a8a62279bc620a59508cce9c9510b0fc4e6fc377
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 18:25:47 +0000

    v18: حذف @ از بج سازنده

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v17';
+const CACHE = 'karkard-v18';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
