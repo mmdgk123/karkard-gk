a494f693f7d1b62d145cc6692d615c12bd2d2f2f
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 18:07:22 +0000

    v15: فیکس موندن مقدار قبلی توی فرم + پاک‌سازی سه‌لایه

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v14';
+const CACHE = 'karkard-v15';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
