45e9b342f7df7d67fcb505f90ccca9c30194f7f3
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 17:57:56 +0000

    v11: دریافتی سراسری با منبع دلخواه، بدون پروژه

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v10';
+const CACHE = 'karkard-v11';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
