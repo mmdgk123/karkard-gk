ae1259a9898aa8fad6a77ce605338b998595dc05
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 19:12:58 +0000

    v27: دیفالت دسته غذا + آیدی سازنده بالا وسط

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v26';
+const CACHE = 'karkard-v27';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
