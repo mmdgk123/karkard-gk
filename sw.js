e0702550337909ec17592771733b4d4d1717cda0
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 17:36:10 +0000

    v6: دکمه بررسی آپدیت

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v5';
+const CACHE = 'karkard-v6';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
