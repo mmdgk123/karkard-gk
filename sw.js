40744db74f805941b2f19be3645be4bf17c34d3e
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-15 13:03:41 +0000

    v1.0 first stable release

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'karkard-v28';
+const CACHE = 'karkard-1.0';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
